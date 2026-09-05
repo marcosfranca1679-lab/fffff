933b83451ceb036bf0dd39b38804e9e63c601801
marcosfranca1679-lab <marcosfranca1679@gmail.com>
2026-09-04 23:12:02 -0300

    Use icon.ico in BrowserWindow

--- electron-app/main.js
+++ electron-app/main.js
@@ -10,7 +10,7 @@ function createWindow() {
     minWidth: 420,
     minHeight: 600,
     title: '5DAY MC',
-    icon: path.join(__dirname, 'assets', 'icon.png'),
+    icon: path.join(__dirname, 'assets', 'icon.ico'),
     backgroundColor: '#0f0f23',
     autoHideMenuBar: true,
     show: false,
