150570ab0f16c17885ee54dd33742e71f2bad2de
marcosfranca1679-lab <marcosfranca1679@gmail.com>
2026-09-14 21:03:00 -0300

    fix(plugin): add missing import java.util.Collection

--- plugin-src/src/main/java/com/mapabermuda/whitelist/WhitelistPlugin.java
+++ plugin-src/src/main/java/com/mapabermuda/whitelist/WhitelistPlugin.java
@@ -56,6 +56,7 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
